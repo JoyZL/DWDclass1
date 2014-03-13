3be82906c8fb6ad34f735a4e6cb8c48c794ba0b5
Joy Li <joyli221@gmail.com>
2014-03-12 20:56:59 -0400

    heroku app
    heroku app

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-//create a variable, node has a commend line for var - "requre"
 var http = require('http');
-
-//create a function to save a value for creating server, function(request, respond)
+console.log('Hello, starting server...');
 var server = http.createServer(function(req,res){
 res.writeHead(200, {'Content-Type': 'text/html'});
 res.end ('<h1>Hello World</h1>');
 });
 
-server.listen(3000, 'localhost');
+var port = Number(progress.env.PORT || 5000);
+console.log('Listening on port',port);
+server.listen(port);
+
